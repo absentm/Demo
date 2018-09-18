47296293481db62f7c64c6906a7aa9467de8480d
absentm <absentm@163.com>
2018-09-18 13:44:05 +0800

    Update test_args.py

--- Python-demo/args-demo/test_args.py
+++ Python-demo/args-demo/test_args.py
@@ -27,7 +27,7 @@ def greet_me(**kwargs):
     test kwargs
     """
     for key, value in kwargs.items():
-        print "{0} == {1}".format(key, value)
+        print "{0} : {1}".format(key, value)
 
 
 if __name__ == '__main__':
